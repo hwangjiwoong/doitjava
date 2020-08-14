50ef7a31a1f83e76a54a13d8b4ecef9aa417b71d
hwangjiwoong <jiwoong.HJW@gmail.com>
2020-08-14 13:01:15 +0900

    chapter02 - 실수 자료형 예제

---
+++ doitjava/src/chapter022/Charex1.java
@@ -0,0 +1,27 @@
+package chapter022;
+
+public class Charex1 {
+
+	public static void main(String[] args) {
+		char ch1='A';
+		System.out.println(ch1);
+//		ch1='bc'; char 자료형은 1개외 문자만 입력 가능합니다
+		int num1=ch1;
+		System.out.println(num1); //대문자'A'는 숫자 65와 같습니다.
+		char ch2=66;
+		System.out.println(ch2);
+		char ch3=67;
+		System.out.println(ch3);
+		char ch4='a';
+		char num4=ch4;
+		System.out.println(num4);
+		
+		System.out.println('a'-'A');
+		
+		
+		
+		
+
+	}
+
+}
---
+++ doitjava/src/chapter022/DoudleEx1.java
@@ -0,0 +1,14 @@
+package chapter022;
+
+public class DoudleEx1 {
+
+	public static void main(String[] args) {
+		double dnum=3.14123456789012345;	//소수점 16자리
+		float fnum=3.14123456789012345F;	//소수점 7자리
+		
+		System.out.println(dnum);
+		System.out.println(fnum);
+
+	}
+
+}
